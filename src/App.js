96f0c1f750c643db3ac9b218fd5f874ee926156b
Uthaeus <romanlavery@gmail.com>
2023-03-03 19:00:30 -0700

    created basic portfolio container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import PortfolioContainer from './component/portfolio-container';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Homer Simpson's Portfolio</h1>
+      <PortfolioContainer />
     </div>
   );
 }
---
+++ src/component/portfolio-container.js
@@ -0,0 +1,13 @@
+
+
+const PortfolioContainer = props => {
+
+    return (
+        <>
+            <h2>Portfolio items go here...</h2>
+        </>
+    );
+};
+
+
+export default PortfolioContainer;
