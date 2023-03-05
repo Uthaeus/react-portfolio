ef5d1629303f107b5ca8bea73e122b12cfe83149
Uthaeus <romanlavery@gmail.com>
2023-03-04 18:34:13 -0700

    created variables styles and began navigation styling

--- src/component/navigation/navigation-container.js
+++ src/component/navigation/navigation-container.js
@@ -3,28 +3,41 @@ import { NavLink } from "react-router-dom";
 
 const NavigationComponent = props => {
     return (
-        <div>
-            <NavLink to='/' className={({ isActive }) =>
-                isActive ? 'nav-link-active' : undefined
-              } end>
-                Home
-            </NavLink>
-            <NavLink to='/about-me' className={({ isActive }) =>
-                isActive ? 'nav-link-active' : undefined
-              }>
-                About
-            </NavLink>
-            <NavLink to='/contact' className={({ isActive }) =>
-                isActive ? 'nav-link-active' : undefined
-              }>
-                Contact
-            </NavLink>
-            <NavLink to='/blog' className={({ isActive }) =>
-                isActive ? 'nav-link-active' : undefined
-              }>
-                Blog
-            </NavLink>
-            {true ? <NavLink>Add Blog</NavLink> : null}
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink to='/' className={({ isActive }) =>
+                        isActive ? 'nav-link-active' : undefined
+                    } end>
+                        Home
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/about-me' className={({ isActive }) =>
+                        isActive ? 'nav-link-active' : undefined
+                    }>
+                        About
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/contact' className={({ isActive }) =>
+                        isActive ? 'nav-link-active' : undefined
+                    }>
+                        Contact
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/blog' className={({ isActive }) =>
+                        isActive ? 'nav-link-active' : undefined
+                    }>
+                        Blog
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="right-side">
+                Homer Simpson
+            </div>
         </div>
     );
 };
