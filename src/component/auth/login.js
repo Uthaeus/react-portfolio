ec2415aee4b4c76966cb17fee5300c9acdf955c2
Uthaeus <romanlavery@gmail.com>
2023-03-05 11:20:00 -0700

    handling errors on api authentication in login form

--- src/component/auth/login.js
+++ src/component/auth/login.js
@@ -1,26 +1,52 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorText, setErrorText] = useState('');
 
     const handleSubmit = event => {
+        axios.post('https://api.devcamp.space/sessions',
+        {
+            client: {
+                email: email,
+                password: password
+            }
+        },
+        { withCredentials: true }
+        ).then(response => {
+            if (response.data.status === 'created') {
+
+            } else {
+                setErrorText('Wrong email or password');
+            }
+        }).catch(error => {
+            setErrorText('An error occurred')
+        })
         event.preventDefault();
 
     }
 
     const handleEmailChange = event => {
+        if (errorText !== '') {
+            setErrorText('');
+        }
         setEmail(event.target.value);
     }
 
     const handlePasswordChange = event => {
+        if (errorText !== '') {
+            setErrorText('');
+        }
         setPassword(event.target.value);
     }
 
     return (
         <div>
             <h1>Login to access your dashboard</h1>
-            
+            <p>{errorText}</p>
+
             <form onSubmit={handleSubmit}>
                 <input 
                     type='email' 
