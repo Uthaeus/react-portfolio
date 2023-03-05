e2adda4c5dea6f021a966c061bcaabc9c8ba3284
Uthaeus <romanlavery@gmail.com>
2023-03-05 10:59:14 -0700

    working on form functionality

--- src/component/auth/login.js
+++ src/component/auth/login.js
@@ -1,13 +1,45 @@
+import { useState } from "react";
 
+const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+
+    }
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+    }
+
     return (
         <div>
             <h1>Login to access your dashboard</h1>
             
-            <form>
-                <input type='email' />
-                <input type='password' />
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='email' 
+                    name='email'
+                    placeholder="Your email"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                <input 
+                    type='password' 
+                    name='password'
+                    placeholder="Your password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                 />
+
+                 <div>
+                    <button type='submit'>Login</button>
+                 </div>
             </form>
         </div>
     );
