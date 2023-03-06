2821990b790c29fe48d0d3299003caff6f7f2e8b
Uthaeus <romanlavery@gmail.com>
2023-03-06 10:56:22 -0700

    prelim styling for sidebar list items

--- src/component/portfolio/portfolio-sidebar-list.js
+++ src/component/portfolio/portfolio-sidebar-list.js
@@ -3,17 +3,17 @@
 const PortfolioSidebarList = (props) => {
   const portfolioList = props.data.map((item) => {
     return (
-      <div>
-        <div>
+      <div className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
           <img src={item.thumb_image_url} alt="" />
         </div>
-        <h1>{item.name}</h1>
+        <h1 className="title">{item.name}</h1>
         <h2>{item.id}</h2>
       </div>
     );
   });
 
-  return <div>{portfolioList}</div>;
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
