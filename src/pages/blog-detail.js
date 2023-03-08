f7ea0e6a12cd4541789dcd4851b24efa76529681
Uthaeus <romanlavery@gmail.com>
2023-03-08 11:23:20 -0700

    set up state for blog item in blog detail and set up api call for individual blog item

--- src/pages/blog-detail.js
+++ src/pages/blog-detail.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const BlogDetail = () => {
-    return (
-        <h1>Detail</h1>
-    );
+const BlogDetail = (props) => {
+  const [currentId, setCurrentId] = useState(props.match.params.slug);
+  const [blogItem, setBlogItem] = useState({});
+
+  const getBlogItem = () => {
+    axios.get(
+      `https://romanlavery.devcamp.space/portfolio/portfolio_blogs/${currentId}`
+    ).then(response => {
+
+    }).catch(error => {
+        console.log('getBlogItem error', error);
+    })
+  };
+
+  useEffect(() => {
+    getBlogItem();
+  }, []);
+
+  return <h1>Detail</h1>;
 };
-
 
 export default BlogDetail;
