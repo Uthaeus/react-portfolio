382ba5518dcee355600f70e0994d2b5a31a6b576
Uthaeus <romanlavery@gmail.com>
2023-03-08 16:26:05 -0700

    added custom styles to modal and modal overlay

--- src/component/modals/blog-modal.js
+++ src/component/modals/blog-modal.js
@@ -1,8 +1,24 @@
 import ReactModal from "react-modal";
 
 const BlogModal = (props) => {
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: '800px'
+        },
+        overlay: {
+            backgroundColor: 'rgba(1, 1, 1, 0.75)'
+        }
+    }
+
   return (
     <ReactModal
+        style={customStyles}
       onRequestClose={() => {
         props.handleModalClose();
       }}
