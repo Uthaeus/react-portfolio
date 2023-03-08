1dc047bc4b62b6b5711c38ffacda557df4cdd454
Uthaeus <romanlavery@gmail.com>
2023-03-08 15:22:09 -0700

    set up spinner icon when loading data

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //import { Link } from "react-router-dom";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import BlogItem from "../component/blog/blog-item";
 
@@ -11,7 +12,8 @@ class BlogPage extends Component {
     this.state = {
       blogItems: [],
       totalCount: 0,
-      currentPage: 0
+      currentPage: 0,
+      isLoading: true
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -38,7 +40,8 @@ class BlogPage extends Component {
       .then((response) => {
         this.setState({
           blogItems: response.data.portfolio_blogs,
-          totalCount: response.data.meta.total_records
+          totalCount: response.data.meta.total_records,
+          isLoading: false
         });
       })
       .catch((error) => {
@@ -57,7 +60,14 @@ class BlogPage extends Component {
 
     return (
       <div className="blog-container">
+        
         <div className="content-container">{blogRecords}</div>
+
+        {this.state.isLoading && (
+            <div className="content-loader">
+                <FontAwesomeIcon icon='spinner' spin />
+            </div>
+        )}
       </div>
     );
   }
