a400d9d91dcc16c98055406bdbb27f2f1bdfeac8
Uthaeus <romanlavery@gmail.com>
2023-03-10 11:40:05 -0700

    created page layout structure and styling for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,19 @@
 
+import profilePicture from '../assets/images/auth/login.jpg';
 
 const AboutPage = () => {
     return (
-        <>
-            <h1>About Page</h1>
-        </>
+        <div className="content-page-wrapper">
+            <div className="left-column" style={{
+                background: `url(${profilePicture}) no-repeat`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+            }} />
+
+            <div className="right-column">
+                Bio information....
+            </div>
+        </div>
     );
 };
 
