daad750a29c132728f791fe15b8fc4d52e0a5fae
Uthaeus <romanlavery@gmail.com>
2023-03-09 19:13:51 -0700

    implemented and configured Dropzone into blog form for images

--- src/component/blog/blog-form.js
+++ src/component/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { DropzoneComponent } from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -11,12 +12,37 @@ class BlogForm extends Component {
       title: "",
       blog_status: "",
       content: "",
+      featured_image: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleRichTextEditorChange =
       this.handleRichTextEditorChange.bind(this);
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+  }
+
+  handleFeaturedImageDrop() {
+    return {
+        addedfile: file => this.setState({ featured_image: file })
+    }
+  }
+
+  djsConfig() {
+    return {
+        addRemoveLinks: true,
+        maxFiles: 1
+    }
+  }
+
+  componentConfig() {
+    return {
+        iconFiletypes: ['.jpg', '.png'],
+        showFiletypeIcon: true,
+        postUrl: 'https://httpbin.org/post'
+    }
   }
 
   handleRichTextEditorChange(content) {
@@ -85,6 +111,16 @@ class BlogForm extends Component {
 
         <div className="one-column">
           <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange} />
+        </div>
+
+        <div className="image-uploaders">
+            <DropzoneComponent
+                config={this.componentConfig()}
+                djsConfig={this.djsConfig()}
+                eventHandlers={this.handleFeaturedImageDrop()}
+            >
+                <div className="dz-message">Featured Image</div>
+            </DropzoneComponent>
         </div>
 
         <button className="btn">Save</button>
