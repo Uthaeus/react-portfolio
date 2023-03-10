5bda2c2039835c7804e1b06223ce9ea01ae5be4b
Uthaeus <romanlavery@gmail.com>
2023-03-10 15:30:10 -0700

    began structuring of portfolio-detail page

--- src/component/portfolio/portfolio-detail.js
+++ src/component/portfolio/portfolio-detail.js
@@ -3,30 +3,42 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const PortfolioDetail = (props) => {
-    const currentItem = useParams();
-    const [portfolioItem, setPortfolioItem] = useState({});
+  const currentItem = useParams();
+  const [portfolioItem, setPortfolioItem] = useState({});
 
-    const getPortfolioItem = () => {
-        axios.get(`https://romanlavery.devcamp.space/portfolio/portfolio_items/${currentItem.slug}`)
-        .then(response => {
-            setPortfolioItem(response.data.portfolio_item);
-        }).catch(error => {
-            console.log('getPortfolioItem portfolio-detail error', error);
-        });
-    }
+  const getPortfolioItem = () => {
+    axios
+      .get(
+        `https://romanlavery.devcamp.space/portfolio/portfolio_items/${currentItem.slug}`
+      )
+      .then((response) => {
+        setPortfolioItem(response.data.portfolio_item);
+      })
+      .catch((error) => {
+        console.log("getPortfolioItem portfolio-detail error", error);
+      });
+  };
 
-    useEffect(() => {
-        getPortfolioItem();
-    }, []);
+  useEffect(() => {
+    getPortfolioItem();
+  }, []);
 
-    const { id, name, description, url } = portfolioItem;
+  const {
+    id,
+    name,
+    description,
+    url,
+    banner_image_url,
+    category,
+    logo_url,
+    thumb_image_url,
+  } = portfolioItem;
 
-    return (
-        <>
-        <h1>Portfolio Detail for {id}</h1>
-        </>
-    );
+  return (
+    <div>
+        <h2>{name}</h2>
+    </div>
+  );
 };
-
 
 export default PortfolioDetail;
