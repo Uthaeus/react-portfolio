87f7865744203d8a2e94e34d94c31cba81a6a84c
Uthaeus <romanlavery@gmail.com>
2023-03-10 15:24:33 -0700

    retrieving data from api for portfolio item

--- src/component/portfolio/portfolio-detail.js
+++ src/component/portfolio/portfolio-detail.js
@@ -1,8 +1,30 @@
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const PortfolioDetail = (props) => {
+    const currentItem = useParams();
+    const [portfolioItem, setPortfolioItem] = useState({});
+
+    const getPortfolioItem = () => {
+        axios.get(`https://romanlavery.devcamp.space/portfolio/portfolio_items/${currentItem.slug}`)
+        .then(response => {
+            setPortfolioItem(response.data.portfolio_item);
+        }).catch(error => {
+            console.log('getPortfolioItem portfolio-detail error', error);
+        });
+    }
+
+    useEffect(() => {
+        getPortfolioItem();
+    }, []);
+
+    const { id, name, description, url } = portfolioItem;
+
     return (
-        <h1>Portfolio Detail for {props.match.params.slug}</h1>
+        <>
+        <h1>Portfolio Detail for {id}</h1>
+        </>
     );
 };
 
--- src/component/portfolio/portfolio-item.js
+++ src/component/portfolio/portfolio-item.js
@@ -1,40 +1,41 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const PortfolioItem = (props) => {
-    const { description, thumb_image_url, logo_url } = props.item;
-    const [portfolioItemClass, setPortfolioItemClass] = useState('');
+  const { description, thumb_image_url, logo_url, id } = props.item;
+  const [portfolioItemClass, setPortfolioItemClass] = useState("");
 
-    const handleMouseEnter = () => {
-        setPortfolioItemClass('image-blur');
-    }
+  const handleMouseEnter = () => {
+    setPortfolioItemClass("image-blur");
+  };
 
-    const handleMouseLeave = () => {
-        setPortfolioItemClass('')
-    }
+  const handleMouseLeave = () => {
+    setPortfolioItemClass("");
+  };
 
-    return (
-        <div className="portfolio-item-wrapper"
-            onMouseEnter={() => handleMouseEnter()}
-            onMouseLeave={() => handleMouseLeave()}
-        >
-            <div
-                className={"portfolio-img-background " + portfolioItemClass}
-                style={{
-                    backgroundImage: `url(${thumb_image_url})`
-                }}     
-            />
+  return (
+    <Link to={`/portfolio/${id}`}>
+      <div
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => handleMouseEnter()}
+        onMouseLeave={() => handleMouseLeave()}
+      >
+        <div
+          className={"portfolio-img-background " + portfolioItemClass}
+          style={{
+            backgroundImage: `url(${thumb_image_url})`,
+          }}
+        />
 
-            <div className="img-text-wrapper">
-                <div className="logo-wrapper">
-                    <img src={logo_url} alt='' />
-                </div>
-                <div className="subtitle">
-                    {description}
-                </div>
-            </div>
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} alt="" />
+          </div>
+          <div className="subtitle">{description}</div>
         </div>
-    );
+      </div>
+    </Link>
+  );
 };
-
 
 export default PortfolioItem;
